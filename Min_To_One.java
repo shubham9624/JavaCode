190aa808131850d16e122c67c2876aae814e3df2
shubham9624 <shubhamkrissh88@gmail.com>
2024-05-16 14:53:57 +0530

    Minimun to get 1

---
+++ Min_To_One.java
@@ -0,0 +1,21 @@
+package JavaCode;
+class Min_To_One{
+
+    public static int countMinStepsToOne(int n) {
+		//Your code goes here
+         int[] dp = new int[n + 1];
+        dp[1] = 0; // Base case
+
+        for (int i = 2; i <= n; i++) {
+            
+            dp[i] = dp[i - 1] + 1; 
+            if (i % 2 == 0)
+             dp[i] = Math.min(dp[i], dp[i / 2] + 1); 
+            if (i % 3 == 0)
+             dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
+
+        return dp[n];
+	}
+
+}
