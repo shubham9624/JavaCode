4d18bac031fda1f18d5237eff100ac7ef2b70664
shubham9624 <shubhamkrissh88@gmail.com>
2024-05-18 13:02:12 +0530

    Take BinaryTree class outside

--- Create_Binary_Tree_Inorde_with_postOrde.java
+++ Create_Binary_Tree_Inorde_with_postOrde.java
@@ -1,17 +1,18 @@
 package JavaCode;
 import java.util.*;
-public class Create_Binary_Tree_Inorde_with_postOrde {
-    class BinaryTreeNode<T> {
-		T data;
-		BinaryTreeNode<T> left;
-		BinaryTreeNode<T> right;
+class BinaryTreeNode<T> {
+    T data;
+    BinaryTreeNode<T> left;
+    BinaryTreeNode<T> right;
 
-		public BinaryTreeNode(T data) {
-			this.data = data;
-			this.left = null;
-			this.right = null;
-		}
-	}
+    public BinaryTreeNode(T data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+public class Create_Binary_Tree_Inorde_with_postOrde {
+    
     
     public static BinaryTreeNode<Integer> buildTree(int[] postOrder, int[] inOrder) {
 		//Your code goes here
@@ -30,7 +31,7 @@ public class Create_Binary_Tree_Inorde_with_postOrde {
         int element = postOrder[postStart[0]--];
         BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(element);
          int pos = map.get(element);
-         
+
         root.right=buildTree(postOrder , postStart , posEnd , inOrder , pos+1 , inEnd , map);
         root.left=buildTree(postOrder , postStart , posEnd , inOrder , inStart , pos-1 , map);
        
