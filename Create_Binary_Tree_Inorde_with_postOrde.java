11e4565975eed784fc0e821841ab7b518b00d8cc
shubham9624 <shubhamkrissh88@gmail.com>
2024-05-18 12:59:07 +0530

    Buid Tree Inorder with PostOrder

---
+++ Create_Binary_Tree_Inorde_with_postOrde.java
@@ -0,0 +1,45 @@
+package JavaCode;
+import java.util.*;
+public class Create_Binary_Tree_Inorde_with_postOrde {
+    class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+
+		public BinaryTreeNode(T data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+    
+    public static BinaryTreeNode<Integer> buildTree(int[] postOrder, int[] inOrder) {
+		//Your code goes here
+		HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        getIndexofInorder(inOrder, map);
+        int postStart[]={postOrder.length-1};
+        return buildTree(postOrder , postStart, 0 , inOrder , 0 , inOrder.length-1 , map);
+    }
+    
+    private static BinaryTreeNode<Integer> buildTree(int[] postOrder, int postStart[], int posEnd, int[] inOrder, int inStart, int inEnd, HashMap<Integer,Integer> map) {
+
+        if(postStart[0] < posEnd || inStart > inEnd)
+        {
+            return null;
+        }
+        int element = postOrder[postStart[0]--];
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(element);
+         int pos = map.get(element);
+         
+        root.right=buildTree(postOrder , postStart , posEnd , inOrder , pos+1 , inEnd , map);
+        root.left=buildTree(postOrder , postStart , posEnd , inOrder , inStart , pos-1 , map);
+       
+        return root;
+    }
+    private static void getIndexofInorder(int[] inOrder, HashMap<Integer,Integer> map) {
+        for(int i=0; i<inOrder.length; i++)
+        {
+            map.put(inOrder[i], i);
+        }
+    }
+}
